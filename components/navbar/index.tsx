e9dcb55cb575d14a115abbac0d7dfaf201ebe608
Ifan <ifana.andri@upi.edu>
2022-05-17 01:00:31 +0700

    fix style header

--- components/navbar/index.tsx
+++ components/navbar/index.tsx
@@ -3,10 +3,10 @@ export default function navbar(props:any){
         <nav className="navbar navbar-light fixed-top" style={{backgroundColor:'rgba(255, 255, 255, 0.19)'}}>
             <div className="container py-0">
                 <a className="navbar-brand" href="#">
-                    <img src="/logo-header.png" alt="" width="150" className="d-inline-block rounded align-text-top"/>
+                    <img src="/logo-header.png" alt="" width="145" className="d-inline-block rounded align-text-top"/>
                 </a>
                 <div className='d-flex align-items-center'>
-                    <a href="#" className='btn btn-outline-light fw-bold px-4'>
+                    <a href="#" className='btn btn-outline-light fw-bold px-2'>
                         Home
                     </a>
                     <hr className='vr py-3 border border-2 opacity-100 rounded-pill mx-3'/>
