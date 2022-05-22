b35f7f5806fc1b6ca069c559f0e78f1d21dd253f
Ifan <ifana.andri@upi.edu>
2022-05-22 12:06:39 +0700

    update structure

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,13 +2,13 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import Layout from '../components/layout'
 import Navbar from '../components/navbar'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.main}>
-        <Navbar/>
+    <Layout>
         <div className={styles.jumbotron}>
           <Image src={'/halaman-utama-gambar-1.png'} layout="fill" objectFit="cover" quality={100}></Image>
           <div className="container text-white text-center position-absolute top-50 start-50 translate-middle mt-4">
@@ -111,7 +111,7 @@ const Home: NextPage = () => {
               <div className="col-md-8">
                 <label htmlFor="email" className='form-label'>PESAN</label>
                 <textarea name="pesan" className='form-control' id="email" cols={30} rows={10}></textarea>
-                <button type='submit' className='btn btn-light mt-4 w-100'>Submit</button>
+                <button type='submit' className='btn btn-secondary mt-4 w-100'>Submit</button>
               </div>
             </form>
           </div>
@@ -121,47 +121,7 @@ const Home: NextPage = () => {
             <h6>#Jabar ngahiji, jabar kahiji</h6>
           </div>
         </div>
-
-        <div className={styles.footer}>
-          <div className="container py-5 text-white">
-            <div className="d-flex align-items-center mb-4">
-              <img src="phone-call.png" alt="" className='me-3' width={70}/>
-              <div>
-                <p>Jl. A.H. Nasution No.105, Bandung, Jawa Barat 40614</p>
-                <p>+62 899-3734-770</p>
-                <p>Line : @jga4294g</p>
-              </div>
-            </div>
-
-            <div className="d-flex justify-content-between">
-              <div className="d-flex align-items-center justify-content-center">
-                <a href="#" className='btn p-0'>
-                  <img 
-                  src="https://www.freepnglogos.com/uploads/instagram-logo-png-transparent-background-hd-3.png" 
-                  alt="" 
-                  height={'40px'}
-                  className=""
-                  />
-                </a>
-                <a href="#" className='btn p-0 mx-3'>
-                  <img 
-                  src="https://www.freepnglogos.com/uploads/line-messenger-logo-2.png" 
-                  alt="" 
-                  height={'40px'}
-                  />
-                </a>
-                <a href="#" className='btn p-0'>
-                  <img 
-                  src="https://www.freepnglogos.com/uploads/youtube-play-red-logo-png-transparent-background-6.png" 
-                  alt="" 
-                  height={'40px'}/>
-                </a>
-              </div>
-              <img src="logo-footer.png" alt="" width={60}/>
-            </div>
-          </div>
-        </div>
-    </div>
+    </Layout>
   )
 }
 
