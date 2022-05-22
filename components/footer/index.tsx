fc89d12904bc3d34220a1d6b09339720ef1bebbd
Ifan <ifana.andri@upi.edu>
2022-05-22 12:06:52 +0700

    update structure

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -1,7 +1,43 @@
 export default function footer(props:any){
     return(
-        <div>
-            ini footer
+        <div style={{backgroundColor: '#424851'}}>
+          <div className="container py-5 text-white">
+            <div className="d-flex align-items-center mb-4">
+              <img src="phone-call.png" alt="" className='me-3' width={70}/>
+              <div>
+                <p>Jl. A.H. Nasution No.105, Bandung, Jawa Barat 40614</p>
+                <p>+62 899-3734-770</p>
+                <p>Line : @jga4294g</p>
+              </div>
+            </div>
+
+            <div className="d-flex justify-content-between">
+              <div className="d-flex align-items-center justify-content-center">
+                <a href="#" className='btn p-0'>
+                  <img 
+                  src="https://www.freepnglogos.com/uploads/instagram-logo-png-transparent-background-hd-3.png" 
+                  alt="" 
+                  height={'40px'}
+                  className=""
+                  />
+                </a>
+                <a href="#" className='btn p-0 mx-3'>
+                  <img 
+                  src="https://www.freepnglogos.com/uploads/line-messenger-logo-2.png" 
+                  alt="" 
+                  height={'40px'}
+                  />
+                </a>
+                <a href="#" className='btn p-0'>
+                  <img 
+                  src="https://www.freepnglogos.com/uploads/youtube-play-red-logo-png-transparent-background-6.png" 
+                  alt="" 
+                  height={'40px'}/>
+                </a>
+              </div>
+              <img src="logo-footer.png" alt="" width={60}/>
+            </div>
+          </div>
         </div>
     )
 }
--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -1,3 +1,18 @@
-export default function mainLayout(props: any) {
-    
+import { Children, ReactNode } from "react";
+import Footer from "../footer";
+import Navbar from "../navbar";
+
+interface LayoutProps {
+    children: ReactNode
+}
+
+export default function mainLayout(props: LayoutProps) {
+    const {children} = props
+    return(
+        <>
+            <Navbar/>
+            {children}
+            <Footer/>
+        </>
+    )
 }
