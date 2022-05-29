7ab863aefafc36fce097855355c6e8bc54628370
Ifan <ifana.andri@upi.edu>
2022-05-29 17:33:24 +0700

    fix err on api himpunans

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,8 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { himpunans } from '../../data/himpunans'
+
 type Data = {
-  id: BigInteger,
   name: string
 }
 
@@ -10,5 +9,5 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json(himpunans)
+  res.status(200).json({ name: 'John Doe' })
 }
--- pages/api/himpunans/index.tsx
+++ pages/api/himpunans/index.tsx
@@ -1,5 +1,5 @@
 import { himpunans } from '../../../data/himpunans';
 
-export default function handler(req, res){
+export default function handler(req:any, res:any){
     res.status(200).json(himpunans)
 }
