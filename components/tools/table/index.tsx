dfe85eb5dbea7678abaa3ff1e1ada4f33f1ab5c6
Ifan <ifana.andri@upi.edu>
2022-06-05 22:43:12 +0700

    add kepengurusan page also the api

--- components/tools/table/index.tsx
+++ components/tools/table/index.tsx
@@ -1,29 +1,60 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 import { Key, useEffect, useState } from "react"
 
 export default function table({head,content}:any){
     // eslint-disable-next-line react-hooks/rules-of-hooks
-    const [data, setData] = useState()
+    const [data, setData] = useState([])
 
 
-    // eslint-disable-next-line react-hooks/rules-of-hooks
-    useEffect(()=> {
-        console.log('this is useEffect');
-        
-        setData(content)
-        console.log(data);
-        
-    })
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    useEffect(
+        () => {
+            setData(content)
+            console.log(data);
+            
+        }
+    )
     
     
        
     return (
-        <>
+        <table className="table table-bordered table-light table-hover">
+            <thead>
+                <tr>
+                    {
+                        head.map( (d:any,key:Key) => {
+                            return(
+                                <th scope="col" key={key}>{d}</th>
+                            )
+                        })
+                    }
+                </tr>
+            </thead>
+            <tbody>
+
+
+                {
+                    data &&
+                    data.map( (row:any,key:Key) => {
+                        return(
+                            <TableRow datas={row} key={key}/>
+                        )
+                    } )
+                }
+            </tbody>
+        </table>
+    )
+}
+
+const TableRow = ({datas}:any) => {
+    return (
+        <tr>
             {
-                data.array.forEach((data:any) => {
-                    console.log("test");
+                Object.values(datas).map( (data:any,key:Key) => {
+                    if (data != datas.id) return <td key={key}>{data}</td>
                     
                 })
             }
-        </>
+        </tr>
     )
 }
--- pages/profile/kepengurusan.tsx
+++ pages/profile/kepengurusan.tsx
@@ -12,31 +12,103 @@ export default function kepengurusan(){
         kewirus: '',
         psdm: ''
     } )
-    const getExOfficio = async () => {
+    const getAllData = async () => {
         try {
-            const res = await fetch('/api/anggota/exOfficio')
-            .then(
-                res => res.json()
-            )
-            .then(
-                res => {
-                    SetState({...state, exOfficio: res})
-                }
-            )
+            let [exOfficio, advokom, ristek, sosmas, kewirus, psdm] = await Promise.all([
+                fetch('/api/anggota/exOfficio'),
+                fetch('/api/anggota/advokom'),
+                fetch('/api/anggota/ristek'),
+                fetch('/api/anggota/sosmas'),
+                fetch('/api/anggota/kewirus'),
+                fetch('/api/anggota/psdm')
+            ])
+            
+            SetState({
+                exOfficio: await exOfficio.json(),
+                advokom: await advokom.json(),
+                ristek: await ristek.json(),
+                sosmas: await sosmas.json(),
+                kewirus: await kewirus.json(),
+                psdm: await psdm.json(),
+            })
+            
+            
+            
+            
         } catch (error) {
-            console.log(error);
+            console.log('error bro');
         }
     }
 
+
     // eslint-disable-next-line react-hooks/rules-of-hooks
     useEffect(()=> {
-        getExOfficio()
+       getAllData()
     },[])
 
     return(
         <Layout solidColor={true}>
             <div className="container py-5 my-5">
-                <Table head={['Nama', 'Jabatan', 'Himpunan']} content={state.exOfficio}/>
+                <h1 className="my-3">Kepengurusan</h1>
+                <div className="row py-2">
+                    <div className="col-md-2">
+                        <h3>Ex-Officio</h3>
+                    </div>
+                    <div className="col-md-10 d-flex">
+                        <div className="me-md-3">:</div>
+                        <Table head={['Nama', 'Jabatan', 'Himpunan']} content={state.exOfficio}/>
+                    </div>
+                </div>
+
+                <div className="row py-2">
+                    <div className="col-md-2">
+                        <h3>Advokom</h3>
+                    </div>
+                    <div className="col-md-10 d-flex">
+                        <div className="me-md-3">:</div>
+                        <Table head={['Nama', 'Jabatan', 'Himpunan']} content={state.advokom}/>
+                    </div>
+                </div>
+
+                <div className="row py-2">
+                    <div className="col-md-2">
+                        <h3>Ristek</h3>
+                    </div>
+                    <div className="col-md-10 d-flex">
+                        <div className="me-md-3">:</div>
+                        <Table head={['Nama', 'Jabatan', 'Himpunan']} content={state.ristek}/>
+                    </div>
+                </div>
+
+                <div className="row py-2">
+                    <div className="col-md-2">
+                        <h3>Sosmas</h3>
+                    </div>
+                    <div className="col-md-10 d-flex">
+                        <div className="me-md-3">:</div>
+                        <Table head={['Nama', 'Jabatan', 'Himpunan']} content={state.sosmas}/>
+                    </div>
+                </div>
+
+                <div className="row py-2">
+                    <div className="col-md-2">
+                        <h3>Kewirus</h3>
+                    </div>
+                    <div className="col-md-10 d-flex">
+                        <div className="me-md-3">:</div>
+                        <Table head={['Nama', 'Jabatan', 'Himpunan']} content={state.kewirus}/>
+                    </div>
+                </div>
+
+                <div className="row py-2">
+                    <div className="col-md-2">
+                        <h3>PSDM</h3>
+                    </div>
+                    <div className="col-md-10 d-flex">
+                        <div className="me-md-3">:</div>
+                        <Table head={['Nama', 'Jabatan', 'Himpunan']} content={state.psdm}/>
+                    </div>
+                </div>
             </div>
         </Layout>
     )
